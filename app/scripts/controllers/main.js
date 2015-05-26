1baf4f0f4332240dd065503e22d00ff09599e60e
mydearxym <mydearxym@qq.com>
2015-05-26 10:18:50 +0800

    angular velocity test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,77 +5,19 @@
 
 var myapp = angular.module('myappApp', [
   'ngAnimate',
-  'ui.bootstrap'
-]);
-
-myapp.service("validService", function($q){
-  var self = this;
-
-  self.eq1 = function(number){
-    var deferred = $q.defer();
-
-    if(number == 1){
-      deferred.resolve(" = 1");
-    } else {
-      deferred.reject(" != 1");
-    }
-
-    return deferred.promise;
-  }
-
-  self.lt3 = function(number){
-    var deferred = $q.defer();
-
-    if(number >= 3){
-      deferred.resolve(" > 3 !");
-    } else {
-      deferred.reject(" < 3 ");
-    }
-
-    return deferred.promise;
-  }
-
-});
+  'ui.bootstrap',
+  'angular-velocity'
+  // 'formFor.materialTemplates'
+  // 'formFor.bootstrapTemplates'
+  ]);
 
 
-myapp.controller("MainController", function($q, $log, validService, $scope){
+
+myapp.controller("MainController", function($q, $log, $scope, $timeout){
   var self = $scope;
 
 
-  self.fruits = ["Apple","Apricot","Avocado","Banana","Breadfruit","Bilberry","Blackberry","Blackcurrant","Blueberry","Boysenberry","Cantaloupe","Currant","Cherry","Cherimoya","Cloudberry","Coconut","Cranberry","Cucumber","Damson","Date","Dragonfruit","Durian"];
-
-  self.removeit = function(item){
-    console.log("removeit: ", item);
-    _.remove(self.fruits, function(v){
-      return v == item
-    })
-
-  }
-
-  self.articles = [
-    '11111',
-    '22222',
-    '33333',
-    'jfiejf',
-    '11121',
-    '11141',
-    '1111h1',
-    '1111fei1'
-  ]
 
 
-
-  self.isCollapsed2 = false;
 });
-
-
-
-
-
-
-
-
-
-
-
 
