e63f49a291877d490aa0c2c12ed01d3f9c92bd1a
mydearxym <mydearxym@qq.com>
2015-06-09 14:54:10 +0800

    可以工作的版本。

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,8 +16,22 @@ var myapp = angular.module('myappApp', [
 myapp.controller("MainController", function($q, $log, $scope, $timeout){
   var self = $scope;
 
+  self.items1 = ["djfei", "djfei2", "djfei3", "djfei4", "djfei5", "djfei6", "djfei7", "djfei8", "djfei9"]
+  self.items2 = ["123", "456", "789", "131123", "45896", "782329", "4532323", "23958", "18289494"]
 
+  self.items = self.items1
 
+  self.change = function() {
+  	
+  	if( self.items == self.items1) {
+  		self.items = []
+  		self.items = self.items2
+  	} else {
+  		self.items = []
+  		self.items = self.items1
+  	}
+
+  }
 
 });
 
